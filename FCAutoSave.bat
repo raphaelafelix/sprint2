889a31ea3fd8d52d6dd5b6e14085372ecce513b6
raphaela <raphaelafelixdearaujo@gmail.com>
2025-11-05 14:34:19 -0300

    first commit

---
+++ FCAutoSave.bat
@@ -0,0 +1,10 @@
+start https://github.com/?locale=pt-br
+git init
+git status
+git config --global user.name "raphaela"
+git config --global user.email raphaelafelixdearaujo@gmail.com
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/raphaelafelix/sprint2.git
+git push -u origin main
