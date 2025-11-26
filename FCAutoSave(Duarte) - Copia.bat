66f39a195cb638b1a670435aee0d417e8b9ccf8f
joao vitor <lucajovihome@gmail.com>
2025-11-26 14:18:59 -0300

    quarto commit

--- FCAutoSave(Duarte) - Copia.bat
+++ FCAutoSave(Duarte) - Copia.bat
@@ -1,4 +1,7 @@
+git config --global user.email "lucajovihome@gmail.com"
+git config --global user.name "joao vitor"
 git add .
 git commit -m "quarto commit"
+git remote add origin https://github.com/raphaelafelix/sprint2.git
 git branch -M main
 git push -u origin main
